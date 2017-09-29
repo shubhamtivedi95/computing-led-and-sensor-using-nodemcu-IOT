16fd841fe0ae5163ae57a0a0620a2c536a5a0bb2
Shubham Trivedi <shubhamtrivedi95@gmail.com>
2017-09-29 21:58:28 +0530

    Updated

--- main.py
+++ main.py
@@ -2,23 +2,15 @@ from mqtt import MQTTClient
 import machine 
 from network import WLAN, STA_IF
 import time
-led=machine.Pin(2, machine.Pin.OUT)
-button=machine.Pin(12, machine.Pin.IN)
+
+ssid="<your wifi ssid>"
+password="<your wifi password>"
+username="<your username>"
+secrete_key="<active key>"
+read_obj_name="<led block name>"
+write_obj_name="<text block name>"
 wlan=WLAN(STA_IF)
-username="shubhamtrivedi95"
-secrete_key="ca1a19815541489a8a06436ae13e7422"
-read_obj_name="LED"
-write_obj_name="IR"
-file=open('data.txt')
-data=file.read()
-print(data)
-if str(data)=="b'ON'":
-   led.value(0)
-   print("on")
-elif str(data)=="b'OFF'":
-   led.value(1)
-   print("off")
-file.close()
+wlan.active(True)
 
 def sub_cb(topic, msg):
    print("Msg from server:%s"%msg)
@@ -34,6 +26,28 @@ def sub_cb(topic, msg):
       file=open('data.txt','w')
       file.write(str(msg))
       file.close()
+
+while(not wlan.active()):
+   wlan.scan()
+   if not wlan.isconnect():
+      wlan.connect(ssid,password)
+      wlan.ifconfig()
+
+led=machine.Pin(2, machine.Pin.OUT)
+button=machine.Pin(12, machine.Pin.IN)
+
+file=open('data.txt')
+data=file.read()
+print(data)
+if str(data)=="b'ON'":
+   led.value(0)
+   print("on")
+elif str(data)=="b'OFF'":
+   led.value(1)
+   print("off")
+file.close()
+
+
       
 while(wlan.active()):
    client = MQTTClient("my device","io.adafruit.com",0,username,secrete_key) 
@@ -59,5 +73,4 @@ while(wlan.active()):
    print("Leaving main")
    client.disconnect()
    break
-if not wlan.active():
-   print("no wifi found")
+
