393a5599e1918a28fc118646f488e883eec00e09
Shubham Trivedi <shubhamtrivedi95@gmail.com>
2019-02-01 16:29:09 +0530

    WiFi connectivity is improved.

--- main.py
+++ main.py
@@ -1,76 +1,98 @@
+#debug symbol
+debug = 0
+
+#importing the libraries
 from mqtt import MQTTClient 
-import machine 
+import machine
 from network import WLAN, STA_IF
 import time
 
+#configure your data here.
 ssid="<your wifi ssid>"
 password="<your wifi password>"
 username="<your username>"
 secrete_key="<active key>"
 read_obj_name="<led block title>"
 write_obj_name="<text block title>"
-wlan=WLAN(STA_IF)
-wlan.active(True)
 
+#function to establish the connection with WiFi router.
+def make_connection():
+	if not wlan.isconnected():
+		wlan.scan()
+        wlan.connect(ssid,password)
+        while not wlan.isconnected():
+			try:
+				print("ssid {s} is not found".format(s=ssid))
+			except KeyboardInterrupt:
+				break
+			time.sleep(1)
+	if debug:
+		print('network config:', wlan.ifconfig())
+	print("Successfully connected to {}".format(ssid))
+	return wlan.isconnected()
+
+#function to read the data from server.
 def sub_cb(topic, msg):
-   print("Msg from server:%s"%msg)
-   if str(msg) =="b'OFF'":
-      led.value(1)
-      print("off")
-      file=open('data.txt','w')
-      file.write(str(msg))
-      file.close()
-   elif str(msg) =="b'ON'":
-      led.value(0)
-      print("on")
-      file=open('data.txt','w')
-      file.write(str(msg))
-      file.close()
+	msg=msg.decode("utf-8") 
+	if debug:
+		print("Msg from server: {}".format(msg))		
+	if msg is "OFF":
+		led.value(1)
+		print("off")
+	elif msg =="ON":
+		led.value(0)
+		print("on")	
+	file=open('data.txt','w')
+	file.write(str(msg))
+	file.close()
 
-while(not wlan.active()):
-   wlan.scan()
-   if not wlan.isconnect():
-      wlan.connect(ssid,password)
-      wlan.ifconfig()
-
-led=machine.Pin(2, machine.Pin.OUT)
+led=machine.Pin(16, machine.Pin.OUT)
 button=machine.Pin(12, machine.Pin.IN)
 
 file=open('data.txt')
 data=file.read()
-print(data)
-if str(data)=="b'ON'":
-   led.value(0)
-   print("on")
-elif str(data)=="b'OFF'":
-   led.value(1)
-   print("off")
 file.close()
+if data is "ON":
+	led.value(0)
+	print("on")
+elif data is "OFF":
+	led.value(1)
+	print("off")
 
-
+#turn on WiFi
+wlan = WLAN(STA_IF)
+wlan.active(True)
+make_connection()
       
-while(wlan.active()):
-   client = MQTTClient("my device","io.adafruit.com",0,username,secrete_key) 
-   client.set_callback(sub_cb) 
-   client.connect()
-   client.subscribe(topic=username+"/feeds/"+read_obj_name)
-   i="No Obstracle Detected"
-   print("Sending %s"%str(i)) 
-   client.publish(topic=username+"/feeds/"+write_obj_name, msg=str(i))
-   while True:
-      try:
-         val=button.value()
-         if val == 0:
-            i="Obstracle Detected"
-            print("Sending %s"%str(i)) 
-            client.publish(topic=username+"/feeds/"+write_obj_name, msg=str(i))
-         time.sleep(1)
-         print(client.check_msg())
-         print(str(val))
-      except KeyboardInterrupt:
-         break
+#infinite loop
+while True:
+	if wlan.isconnected():
+		client = MQTTClient("my device","io.adafruit.com",0,username,secrete_key) 
+		client.set_callback(sub_cb) 
+		client.connect()
+		client.subscribe(topic=username+"/feeds/"+read_obj_name)
+		i="No Obstracle Detected"
+		print("Sending %s"%str(i)) 
+		client.publish(topic=username+"/feeds/"+write_obj_name, msg=str(i))
+	while wlan.isconnected():
+		try:
+			val=button.value()
+			if val == 0:
+				i="Obstracle Detected"
+				print("Sending %s"%str(i)) 
+				client.publish(topic=username+"/feeds/"+write_obj_name, msg=str(i))
+			time.sleep(0.5)
+			resp=client.check_msg()
+			if debug:
+				print(resp)
+				print(str(val))
+		except KeyboardInterrupt:
+			break
+	while not make_connection():
+		pass
+	print("Leaving main")
+	client.disconnect()
+	break
 
-   print("Leaving main")
-   client.disconnect()
-   break
+
 
